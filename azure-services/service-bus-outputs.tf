aa8473c6290ad25193ddaef2e34e9f5698263937
Waleed Gadelkareem <gadelkareem@gmail.com>
2024-02-17 13:23:18 +0100

    refactor

--- azure-services/service-bus-outputs.tf
+++
@@ -1,6 +0,0 @@
-##################################################################################
-# OUTPUTS
-##################################################################################
-output "primary_connection_string " {
-  value = "${azurerm_servicebus_queue_authorization_rule.root.primary_connection_string}"
-}
